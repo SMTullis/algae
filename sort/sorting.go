b7e890cc3d58eab545fe4f4d5e7677986e134808
SMTullis <seantullis@vivaldi.net>
2019-03-23 11:02:27 -0600

    Add sort import, isInArray(), and update PigeonSort()
    PigeonSort needs to sort the map. To do this, I have added a slice containing each unique key in the map. This slice is sorted prior to generating the final output.
    
    isInArray() checks for the existance of a value in the array.

--- sort/sorting.go
+++ sort/sorting.go
@@ -1,4 +1,6 @@
-package sort
+package sorting
+
+import "sort"
 
 func MergeSort(arr []int) []int {
     length := len(arr)
@@ -32,14 +34,26 @@ func merge(front, back []int) []int {
 
 func PigeonSort(unsorted []string) []string {
     holes := make(map[string][]string)
+    sorted_keys := make([]string, 0)
     for _, value := range unsorted {
         holes[value] = append(holes[value], value)
+        if !isInArray(sorted_keys, value) {
+            sorted_keys = append(sorted_keys, value)
+        }
     }
+    sort.Strings(sorted_keys)
     var out []string
-    for _, each := range holes {
-        for _, x := range each {
+    for _, each := range sorted_keys {
+        for _, x := range holes[each] {
             out = append(out, x)
         }
     }
     return out
+}
+
+func isInArray(array []string, value string) bool {
+    for _, each := range array {
+        if each == value { return true }
+    }
+    return false
 }
